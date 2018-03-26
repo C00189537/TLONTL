6ab2e33208c7cf78d0d4dda70bd86b71bae75376
Amanda <a_sukhai3@hotmail.com>
2018-03-26 11:50:32 +0200

    worked on tutorial: tag of the platform changes depending on the input of the therapist input. The tag influences the action performed by the player. Stepping track TBD

--- FYP/Assets/_Scripts/Tutorial/NetworkTutorial.cs
+++ FYP/Assets/_Scripts/Tutorial/NetworkTutorial.cs
@@ -4,13 +4,45 @@ using UnityEngine;
 
 public class NetworkTutorial : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    public DFlowNetwork network;
+
+    public float nLeaning = 0;
+    public float nOneLeg = 0;
+    public float nStepping = 0;
+    public float nJumping = 0;
+    public float nJumpingOneleg = 0;
+
+    // Use this for initialization
+    void Start () {
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+        nLeaning = network.getOutput(4);
+        nOneLeg = network.getOutput(5);
+        nStepping = network.getOutput(6);
+        nJumping = network.getOutput(7);
+        nJumpingOneleg = network.getOutput(8);
+
+
+        if (nLeaning == 1)
+        {
+            this.tag = "Lean";
+        } else if (nOneLeg == 1)
+        {
+            this.tag = "OneLeg";
+        } else if (nStepping == 1)
+        {
+            this.tag = "Step";
+        } else if (nJumping == 1)
+        {
+            this.tag = "Jump";
+        }
+        else if (nJumpingOneleg == 1)
+        {
+            this.tag = "Jump2";
+        }
+
+    }
 }
