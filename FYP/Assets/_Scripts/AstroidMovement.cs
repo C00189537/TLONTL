1d5b93a48bff463838100cae2d4cf78a7e220928
Amanda <a_sukhai3@hotmail.com>
2018-03-28 16:31:41 +0200

    Created an astroidbelt

---
+++ FYP/Assets/_Scripts/AstroidMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AstroidMovement : MonoBehaviour {
+
+    WorldController theWorld;
+    public float posZ;
+    public float killpoint = -30;
+
+    // Use this for initialization
+    void Start()
+    {
+        GameObject gameControllerObject = GameObject.FindWithTag("GameController");
+        if (gameControllerObject != null)
+        {
+            theWorld = gameControllerObject.GetComponent<WorldController>();
+        }
+        else if (theWorld == null)
+        {
+            Debug.Log("cannot find 'GameController' script");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        posZ = gameObject.transform.localPosition.z - theWorld.speed/2;
+        gameObject.transform.localPosition = new Vector3(gameObject.transform.localPosition.x, gameObject.transform.localPosition.y, posZ); 
+
+        if (gameObject.transform.position.y < killpoint)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
