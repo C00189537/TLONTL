533502c078a99099e4899d08a5a70c9a5491101d
Amanda <a_sukhai3@hotmail.com>
2018-04-12 09:56:12 +0200

    Add a small bobbing movement to the collectable on the leaning platform

--- FYP/Assets/_Scripts/World controller/Rotato.cs
+++ FYP/Assets/_Scripts/World controller/Rotato.cs
@@ -2,20 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rotato : MonoBehaviour {
+public class Rotato : MonoBehaviour
+{
 
     public Vector3 rotationVector;
 
-	// Use this for initialization
-	void Start () {
+    float originalY;
+    float FloatStrength = 0.3f;
+
+    // Use this for initialization
+    void Start()
+    {
         Vector3 initialY = new Vector3(0, 90, 0);
         transform.Rotate(initialY);
-    }
-	
-	// Update is called once per frame
-	void Update () {
 
-        
+        this.originalY = this.transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         transform.Rotate(rotationVector * Time.deltaTime);
-	}
+
+        transform.position = new Vector3(transform.position.x, originalY + 0.5f + ((float)Mathf.Sin(Time.time) * FloatStrength), transform.position.z);
+    }
 }
