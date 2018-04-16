c9669d3a5c3f304e0b627bdd9b178325865a45b8
Amanda <a_sukhai3@hotmail.com>
2018-04-16 13:29:48 +0200

    added transperant floating icons in front of each platform and removed the icons on top

---
+++ FYP/Assets/_Scripts/Indicator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class Indicator : MonoBehaviour {
+
+    public SpriteRenderer image;
+    public GameObject player; 
+    float alpha;
+    Color color; 
+    float distance; 
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        
+        image = GetComponentInChildren<SpriteRenderer>();
+        color = GetComponentInChildren<SpriteRenderer>().color;
+    }
+
+    private void Update()
+    {
+        distance = Vector3.Distance(player.transform.position, transform.position);
+        alpha = distance / 200.0f;
+
+        color.a = alpha;
+        image.color = color;
+    }
+
+
+}
