835ad4ec4b3b24fbaeb3962b1d54bf262e5ed7cf
Amanda <a_sukhai3@hotmail.com>
2018-04-19 10:24:57 +0200

    difficulty 5 implemented

--- FYP/Assets/_Scripts/World controller/JumpingOneLeg.cs
+++ FYP/Assets/_Scripts/World controller/JumpingOneLeg.cs
@@ -106,45 +106,27 @@ public class JumpingOneLeg : MonoBehaviour
             theWorld.trackPiece[val].transform.Find("Pad5").gameObject.SetActive(false);
             theWorld.trackPiece[val].transform.Find("Pad6").Translate(2.5f, 0, 0);
         }
-
     }
 
     public void FormationFive(int val)
     {
-        int front = 1;
-        int back = 2;
-        int endP = rand.Next(1, 3);
+        int side = rand.Next(1, 3);
 
-        //Spawns random pads
-        if (front == 1)
-        {
-            theWorld.trackPiece[val].transform.Find("Pad2").Translate(0, 1.0f, 0);
-            theWorld.trackPiece[val].transform.Find("Pad3").gameObject.SetActive(false);
-        }
-        else
-        {
-            theWorld.trackPiece[val].transform.Find("Pad3").Translate(0, 1.0f, 0);
-            theWorld.trackPiece[val].transform.Find("Pad2").gameObject.SetActive(false);
-        }
-        if (back == 1)
-        {
-            theWorld.trackPiece[val].transform.Find("Pad4").Translate(0, 1.0f, 0);
-            theWorld.trackPiece[val].transform.Find("Pad5").gameObject.SetActive(false);
-        }
-        else
-        {
-            theWorld.trackPiece[val].transform.Find("Pad5").Translate(0, 1.0f, 0);
-            theWorld.trackPiece[val].transform.Find("Pad4").gameObject.SetActive(false);
-        }
-        if (endP == 1)
-        {
-            theWorld.trackPiece[val].transform.Find("Pad6").Translate(-2.5f, 0.0f, 0);
-            theWorld.trackPiece[val].transform.Find("Pad1").Translate(2.5f, 0, 0);
-        }
-        else
-        {
-            theWorld.trackPiece[val].transform.Find("Pad6").Translate(2.5f, 0.0f, 0);
+        if (side == 1) {
             theWorld.trackPiece[val].transform.Find("Pad1").Translate(-2.5f, 0, 0);
+            theWorld.trackPiece[val].transform.Find("Pad2").gameObject.SetActive(false);
+            theWorld.trackPiece[val].transform.Find("Pad3").Translate(0, 1, 0);
+            theWorld.trackPiece[val].transform.Find("Pad4").Translate(0, 1, 0);
+            theWorld.trackPiece[val].transform.Find("Pad5").gameObject.SetActive(false);
+            theWorld.trackPiece[val].transform.Find("Pad6").Translate(2.5f, 0, 0);
         }
+        else {
+            theWorld.trackPiece[val].transform.Find("Pad1").Translate(2.5f, 0, 0);
+            theWorld.trackPiece[val].transform.Find("Pad2").Translate(0, 1, 0);
+            theWorld.trackPiece[val].transform.Find("Pad3").gameObject.SetActive(false);
+            theWorld.trackPiece[val].transform.Find("Pad4").gameObject.SetActive(false);
+            theWorld.trackPiece[val].transform.Find("Pad6").Translate(-2.5f, 0, 0);
+        }
+
     }
 }
