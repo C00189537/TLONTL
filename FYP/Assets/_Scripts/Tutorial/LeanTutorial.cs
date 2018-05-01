bb3a06e971f3092b88d0e457d8354a83fbe81124
Amanda <a_sukhai3@hotmail.com>
2018-05-01 21:06:16 +0200

    Made a timer for instructions, a timer for a countdown and started on implementing collectibles for the leaning platform.

---
+++ FYP/Assets/_Scripts/Tutorial/LeanTutorial.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LeanTutorial : MonoBehaviour
+{
+
+    PlayerTutorial player;
+    public Image instructions;
+    public Text timeText;
+    public GameObject collectPrefab; 
+
+    public int instructionsTimer = 10;
+    public int waitTimer = 5;
+    public int nrOfCollect = 0; 
+
+    public bool timer = false;
+    public bool gameStart = false;
+
+    System.Random rand = new System.Random();
+
+    // Use this for initialization
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("PlayerTut").GetComponent<PlayerTutorial>();
+        StartCoroutine(CountDown());
+        instructions.enabled = true; 
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (instructionsTimer <= 0 && !timer)
+        {
+            StopCoroutine(CountDown());
+            instructions.enabled = false;
+            StartCoroutine(Wait());  
+        }
+
+        if (waitTimer <= 0 && !gameStart)
+        {
+            gameStart = true;
+            timeText.text = ""; 
+        }
+
+        if (gameStart && nrOfCollect <= 3)
+        {
+            int xPos = rand.Next(-13, 13);
+            Instantiate(collectPrefab, new Vector3(xPos, 1, -8), Quaternion.identity);
+            nrOfCollect++; 
+        }           
+
+    }
+
+    public IEnumerator CountDown()
+    {
+        while (instructionsTimer > 0)
+        {
+            instructionsTimer--;
+            instructions.enabled = true;
+            yield return new WaitForSeconds(1);
+        }
+    }
+
+    public IEnumerator Wait()
+    {
+        while (waitTimer > 0)
+        {
+            timeText.text = waitTimer.ToString();
+            waitTimer--;
+            timer = true;
+            yield return new WaitForSeconds(1);
+        }
+       
+
+    }
+}
---
+++ FYP/Assets/_Scripts/Tutorial/TutorialWorld.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialWorld : MonoBehaviour
+{
+
+    public PlayerTutorial player;
+
+    public int waitTimer = 3;
+    public int instructionsTimer = 10;
+    public Image theInstructions; 
+    public Image Lean;
+    public Image OneLeg;
+    public Image Step;
+    public Image Jump;
+    public Image Jump2;
+
+    bool showInst = false;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        player = GameObject.FindGameObjectWithTag("PlayerTut").GetComponent<PlayerTutorial>();
+        Lean.enabled = false;
+        OneLeg.enabled = false;
+        Step.enabled = false;
+        Jump.enabled = false;
+        Jump2.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (instructionsTimer <= 0)
+        {
+            showInst = false;
+            StopCoroutine(CountDown());
+         
+            Lean.enabled = false;
+            OneLeg.enabled = false;
+            Step.enabled = false;
+            Jump.enabled = false;
+            Jump2.enabled = false;
+        }
+
+        if (player.lean)
+        {
+           
+            while (instructionsTimer >= 0)
+            {
+                StartCoroutine(CountDown());
+              
+
+            }
+
+        }
+        if (player.oneLeg)
+        {
+            instructionsTimer = 3;
+            // StartCoroutine(CountDown());
+            while (instructionsTimer > 0)
+            {
+                Lean.enabled = false;
+                OneLeg.enabled = true;
+                Step.enabled = false;
+                Jump.enabled = false;
+                Jump2.enabled = false;
+            }
+
+        }
+        if (player.step)
+        {
+            instructionsTimer = 3;
+            /// StartCoroutine(CountDown());
+            while (instructionsTimer > 0)
+            {
+                Lean.enabled = false;
+                OneLeg.enabled = false;
+                Step.enabled = true;
+                Jump.enabled = false;
+                Jump2.enabled = false;
+            }
+
+        }
+        if (player.jump)
+        {
+            instructionsTimer = 3;
+            //  StartCoroutine(CountDown());
+            while (instructionsTimer > 0)
+            {
+                Lean.enabled = false;
+                OneLeg.enabled = false;
+                Step.enabled = false;
+                Jump.enabled = true;
+                Jump2.enabled = false;
+            }
+
+        }
+        if (player.jump2)
+        {
+            instructionsTimer = 3;
+            //  StartCoroutine(CountDown());
+            while (instructionsTimer > 0)
+            {
+                Lean.enabled = false;
+                OneLeg.enabled = false;
+                Step.enabled = false;
+                Jump.enabled = false;
+                Jump2.enabled = true;
+            }
+
+        }
+    }
+
+    public IEnumerator CountDown()
+    {
+        // while (instructionsTimer > 0)
+        // {
+        Debug.Log("it workssss");
+        instructionsTimer--;
+        showInst = true; 
+        yield return new WaitForSeconds(1);
+        //}
+
+
+    }
+
+    public IEnumerator Wait()
+    {
+        waitTimer--;
+        yield return new WaitForSeconds(1);
+
+
+    }
+}
